df12810874de6a77ace3681eccdab40daa632052
Hércules S. S. José <hercules_hssj@outlook.com>
2025-03-14 21:46:11 +0000

    Modelo de dados

--- model.py
+++ model.py
@@ -1,3 +1,30 @@
 """
     Entidades utilizadas na API.
 """
+
+from pydantic import BaseModel
+from datetime import datetime
+
+import enum
+
+"""
+    Classe Agenda, que representa a entidade Agenda na base de dados.
+"""
+class Agenda(BaseModel):
+    id: int
+    titulo: str
+    descricao: str
+    dataInicio: datetime
+    dataFim: datetime
+    local: str
+    estadoAtualAgenda: str
+
+
+"""
+    Enumerador que representa os estados possíveis de uma agenda.
+"""
+class EstadoAgenda(enum.Enum):
+    Recebido = 'RECEBIDO'
+    Confirmado = 'CONFIRMADO'
+    Atendido = 'ATENDIDO'
+    Cancelado = 'CANCELADO'
