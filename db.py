fd6ae4f6101b68075fc8f6031092a86c707b1639
Hércules S. S. José <hercules_hssj@outlook.com>
2025-03-14 21:46:22 +0000

    Buscar agendas da base

--- db.py
+++ db.py
@@ -1,3 +1,16 @@
 """
     Métodos para conexão com a base SQlite.
 """
+
+import sqlite3
+
+from contextlib import closing
+
+"""
+    Método para listar todos os registros da agenda.
+"""
+def buscar_agenda():
+    with closing(sqlite3.connect('agenda.sqlite')) as conn:
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM agenda')
+        return cursor.fetchall()
